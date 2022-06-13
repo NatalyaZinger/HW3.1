c5f02a4097614ac3b3eccac7f274947108bf7280
NatalyaZinger <nata.zinger@gmail.com>
2022-06-13 22:09:26 +0500

    Init commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketCost = 10000;
+        int mile = 20;
+        int milesAmount = ticketCost / mile;
+        System.out.println("Вы заработали " + milesAmount + " миль");
+    }
+}
